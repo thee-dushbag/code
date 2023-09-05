c17a8c47c42704776ecdf8e41bc23101ca033bcd
Simon Nganga <happyfortunes5052@gmail.com>
2023-09-05 14:30:30 +0300

    Added a few commands to cmd_tools and updated the help messages on them

--- lib/mpack/nameep/__init__.py
+++ lib/mpack/nameep/__init__.py
@@ -1,4 +1,4 @@
 from ._nameep import load_yaml_config, load_json_config, Context, run
 from ._cli import _cli as cli
 
-__all__ = "load_yaml_config", "load_json_config", "Context", "run"
+__all__ = "load_yaml_config", "load_json_config", "Context", "run", 'cli'
