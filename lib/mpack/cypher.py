0146583e897823b13ae25be190e3c06e82ae0329
Simon Nganga <happyfortunes5052@gmail.com>
2023-10-06 10:35:06 +0300

    Fixed a bug and added an algorithm for caesars cypher.
    The bug in mpack.bsize module: ensure passed object was a float or
    atleast convertible to a float before the actual conversion in all the
    conversion functions. The cypher algorithm simply rotates characters and
    matching them to a specified one given some rotation length.
    Just a sample example but not the actual module API. Read the module for
    further understanding.
    
    Eg: rotate(text='ABCD', length=4) => 'DEFG'

---
+++ lib/mpack/cypher.py
@@ -0,0 +1,42 @@
+import functools
+import click, typing as ty
+
+ShifterType: ty.TypeAlias = ty.Callable[[str, int], str]
+
+
+def shiftchar(char: str, length: int) -> str:
+    return chr(ord(char) + length)
+
+def shift_range(start_char: str, size: int) -> ShifterType:
+    S = ord(start_char)
+    _t = lambda c: S <= c < (S + size)
+
+    def _shifter(char: str, length: int):
+        c = ord(char)
+        v = (((c + length - S) % size) + S) if _t(c) else c
+        return chr(v)
+
+    return _shifter
+
+
+upper_letter_shift = shift_range("A", 26)
+lower_letter_shift = shift_range("a", 26)
+
+
+def shiftletters(text: str, length: int):
+    upper_shift = functools.partial(upper_letter_shift, length=length)  # type: ignore
+    lower_shift = functools.partial(lower_letter_shift, length=length)  # type: ignore
+    text = "".join(map(lower_shift, text))  # type: ignore
+    return "".join(map(upper_shift, text))  # type: ignore
+
+
+@click.command
+@click.argument("text", type=str)
+@click.option("--length", "-l", default=3, type=int)
+def cli(text: str, length: int):
+    ntext = shiftletters(text, length)
+    click.echo(ntext)
+
+
+if __name__ == "__main__":
+    cli()
