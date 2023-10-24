1387767ab1262506cf103d5735b28f3d2bae0aee
Simon Nganga <happyfortunes5052@gmail.com>
2023-10-24 06:30:45 +0300

    Learning BASH arrays.
    Today I have learned some new tricks in my terminal using bash arrays
    and the associative arrays. The declare keyword using different flags
    lets you get a reference to a variable with a certain name using the -n
    flag, print an array using the -p flag, create an associative array
    using -A flag and create integer keyed arrays using -i flag. There are
    more options I will look at in the future.

---
+++ live/others/bash/help.py
@@ -0,0 +1,14 @@
+from mpack.number_reader import get_reader
+from pathlib import Path
+from string import Template
+from mpack.stream import Stream
+
+
+reader = get_reader('english')
+line_template = Template('This is line $word.')
+filename = Path.cwd() / 'text.txt'
+
+with filename.open('w') as file:
+    with Stream(out_stream=file):
+        for i in range(1, 10):
+            print(line_template.substitute(word=reader(i)))
