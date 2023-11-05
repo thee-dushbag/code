9d68c5734aaa7255a814732a9bb2c662080e0139
Simon Nganga <happyfortunes5052@gmail.com>
2023-11-05 22:02:36 +0300

    Making all code-practices terminal capable.
    From now on, for maintainability and reusability, I will convert all my
    code practices to be terminal capable, ie, they take input from the
    terminal and use them in their computations. The advantage of this, is
    that, in the future, testing this algorithms will not require me to
    change the hard coded inputs just to see how they interact with a
    different set of inputs.

--- live/others/codepractice/twosum.cpp
+++ live/others/codepractice/twosum.cpp
@@ -1,9 +1,19 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <sstream>
+
+/*
+Given an array of integers nums and an integer target,
+find the indices of two integers in nums such that their
+sum equals target.
+Assume there is only one solution, ie only two integers
+in nums can add upto target and they are not equal to each
+other.
+*/
 
 struct Solution {
-  std::vector<int> twoSum(std::vector<int>& nums, int target) {
+  static std::vector<int> twoSum(std::vector<int>& nums, int target) {
     std::vector<int> result;
     for (int a = 0; a < nums.size(); a++) {
       for (int b = 0; b < nums.size(); b++) {
@@ -20,31 +30,61 @@ struct Solution {
   }
 };
 
+template <typename T>
+std::string _tostr_vec(std::vector<T> const& vec) {
+  std::stringstream str;
+  std::size_t size{ vec.size() };
+  str << '[';
+  for (uint i = 0; i < size; i++) {
+    str << vec[i];
+    if (i + 1 < size)
+      str << ", ";
+  }
+  str << "]";
+  return str.str();
+};
+
 template<class T>
-void print(std::vector<T> const& vec) {
-  std::cout << '[';
-  for (const auto& t : vec)
-    std::cout << t << ',';
-  std::cout << "]\n";
+std::ostream& operator<<(std::ostream& out, std::vector<T> const& vec) {
+  out << _tostr_vec(vec);
+  return out;
 }
 
 auto main(int argc, char** argv) -> int {
-  std::vector<int> nums{ 3,2,4 };
-  int target = 6;
-  std::cout << "nums: ";
-  print(nums);
-  std::cout << "target: " << target << '\n';
-  Solution sol;
-  auto indicies = sol.twoSum(nums, target);
-  std::cout << "indicies: ";
-  print(indicies);
+  if (argc <= 3) {
+    std::cout << "Usage:\n";
+    std::cout << '\t' << argv[0] << " [input...] target\n";
+    std::cout << "where input and target are integers.\n";
+    std::cout << "None integer values will be substituted for zeros.\n";
+    std::cout << "Example: " << argv[0] << " 1 2 3 5 7\n";
+    std::cout << "Output:";
+    std::cout << R"(
+  input    : [1, 2, 3, 5]
+  target   : 7
+  indicies : [1, 3]
+  output   : [2, 5]
+)";
+    std::exit(1);
+  }
+  std::vector<int> nums;
+  nums.reserve(argc);
+
+  for (int i = 1; i < argc; i++)
+    nums.push_back(std::atoi(argv[i]));
+
+  int target = nums.back();
+  nums.pop_back();
+  auto indicies = Solution::twoSum(nums, target);
   std::vector<int> numbers;
+
   std::transform(
     std::cbegin(indicies),
     std::cend(indicies),
     std::back_inserter(numbers),
     [&nums](int const& index) { return nums[index]; }
   );
-  std::cout << "numbers: ";
-  print(numbers);
+  std::cout << "input    : " << nums << '\n';
+  std::cout << "target   : " << target << '\n';
+  std::cout << "indicies : " << indicies << '\n';
+  std::cout << "output   : " << numbers << '\n';
 }
