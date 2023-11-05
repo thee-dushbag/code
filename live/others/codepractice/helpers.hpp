45c642b516ad26d554917ee5d02ad988a942d0e6
Simon Nganga <happyfortunes5052@gmail.com>
2023-11-06 00:39:30 +0300

    Solved Another LeetCode Problem.
    Unfortunately this problem took me too long such that leetcode did not
    count it as a solved problem, but identified my participation.
    TODO: Keep time in these practice quizes
    The solution was still amazing to come up with. Also I have separated
    code that can be reused in other quizes to the header file `helpers.hpp`

---
+++ live/others/codepractice/helpers.hpp
@@ -0,0 +1,30 @@
+#ifndef __CODEPRACTICE_HEADER_
+#define __CODEPRACTICE_HEADER_
+
+#include <sstream>
+#include <string>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::string _tostr_vec(std::vector<T> const& vec) {
+  std::stringstream str;
+  std::size_t size{ vec.size() };
+  str << '[';
+  for (uint i = 0; i < size; i++) {
+    str << vec[i];
+    if (i + 1 < size)
+      str << ", ";
+  }
+  str << "]";
+  return str.str();
+};
+
+template<class T>
+std::ostream& operator<<(std::ostream& out, std::vector<T> const& vec) {
+  out << _tostr_vec(vec);
+  return out;
+}
+
+
+#endif //__CODEPRACTICE_HEADER_
