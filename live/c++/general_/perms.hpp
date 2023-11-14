20895a1a340e167c81df819577024c68c719cb57
Simon Nganga <happyfortunes5052@gmail.com>
2023-11-14 17:38:26 +0300

    Fixes here and there. BitMask error.
    Fixed some few bugs involving by simple python module`mpack.flags` which
    involved function`flag_off`. This was used to turn a certain flag off in
    some numeric value using the XOR operator, operator`^`. The problem was
    that, if the flag is off in the given numeric value, the flag would then
    be turned on and if on, then off, a toggle effect that I didn't know
    about.
    Possible errors would include Permission elevation, State switch, etc.
    Example:
    	enum Perm { READ = 1 << 0, WRITE = 1 << 1, EXECUTE = 1 << 2 };
    	int perm = READ & WRITE; // involves perm 1 and 2
    	perm = perm ^ READ; // permission is dropped to only 2
    	perm = perm ^ READ; // this activates 1
    	// the intended operation was to turn off READ permission if on
    To prevent this, check if the permission is on first before switching it
    off, except when this toggle behaviour is intended.
    
    Also, the module`mpack.this` cannot be imported if there is no command
    line integer argument $1, fixed this by dropping the need for import
    variable`sys.argv` which seemed to work perfectly in the one lined code.

---
+++ live/c++/general_/perms.hpp
@@ -0,0 +1,107 @@
+#ifndef __SNN_PERMISSIONS_TEST_HPP_
+#define __SNN_PERMISSIONS_TEST_HPP_
+
+#include <iostream>
+
+namespace snn::perms {
+  enum class Perm : u_short {
+    NONE = 0L,
+    READ = 1L << 0,
+    WRITE = 1L << 1,
+    EXECUTE = 1L << 2
+  };
+
+  std::ostream &operator<<(std::ostream &out, Perm const &p) {
+    std::string opname = "UNKNOWN Perm";
+    switch (p) {
+    case Perm::NONE:
+      opname = "Perm::NONE";
+      break;
+    case Perm::READ:
+      opname = "Perm::READ";
+      break;
+    case Perm::WRITE:
+      opname = "Perm::WRITE";
+      break;
+    case Perm::EXECUTE:
+      opname = "Perm::EXECUTE";
+      break;
+    }
+    return out << '<' << opname << ' ' << u_short(p) << '>';
+  }
+
+  struct Permission {
+    Permission() : perm{ u_short(Perm::NONE) } { }
+    Permission(u_short p): perm { p } { }
+    Permission(Perm const &val) : perm{ u_short(val) } { }
+    Permission(Permission const &p) : perm{ p.perm } { }
+    Permission(Permission &&p) : perm{ std::move(p.perm) } {
+      p.perm = u_short(Perm::NONE);
+    }
+    bool operator==(Permission const &p) const {
+      return p.perm == perm;
+    }
+    bool operator!=(Permission const &p) const {
+      return p.perm != perm;
+    }
+    void operator=(Permission const &p) {
+      perm = p.perm;
+    }
+    void operator=(Permission &&p) {
+      perm = p.perm;
+      p.perm = u_short(Perm::NONE);
+    }
+    bool operator==(Perm const &p) const {
+      return has(p);
+    }
+    bool operator!=(Perm const &p) const {
+      return not has(p);
+    }
+    bool has(Perm const &p) const {
+      return (perm & u_short(p)) == u_short(p);
+    }
+    Permission &give(Perm const &p) {
+      perm |= u_short(p); return *this;
+    }
+    Permission &take(Perm const &p) {
+      if (has(p)) perm ^= u_short(p);
+      return *this;
+    }
+    bool only(Perm const &p) const {
+      return perm == u_short(p);
+    }
+    bool has(u_short const &p) const {
+      return (perm & p) == u_short(p);
+    }
+    Permission &give(u_short const &p) {
+      perm |= p; return *this;
+    }
+    Permission &take(u_short const &p) {
+      if (has(p)) perm ^= p;
+      return *this;
+    }
+    bool only(u_short const &p) const {
+      return perm == p;
+    }
+    operator bool() const {
+      return not only(Perm::NONE);
+    }
+    u_short value() const { return perm; }
+    operator u_short() const { return perm; }
+  private:
+    u_short perm;
+  };
+
+  std::ostream &operator<<(std::ostream &out, Permission const &p) {
+    out << "Permission(perm=" << p.value() << ", perms=[";
+    if (p) {
+      if (p.has(Perm::READ)) out << Perm::READ << ',';
+      if (p.has(Perm::WRITE)) out << Perm::WRITE << ',';
+      if (p.has(Perm::EXECUTE)) out << Perm::EXECUTE << ',';
+    }
+    else out << Perm::NONE;
+    return out << "])";
+  }
+}
+
+#endif //__SNN_PERMISSIONS_TEST_HPP_
