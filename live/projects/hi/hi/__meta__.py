257a823fc60bebecda224af8f0679a1e3f114a08
Simon Nganga <happyfortunes5052@gmail.com>
2023-11-17 19:56:06 +0300

    Tried to publish the hi python package.
    I tried to publish my beginner level python package without any success.
    According to the PyPI documentation, I have to register an account to
    TestPyPI and PyPI, generate API tokens then use twine to compile my
    package to uploadable format, ie .tar.gz and .whl files. Use twine to
    upload to the PyPI index. The problem is, this approach fails all the
    time. I have tried it multiple times with very little success.
    And by the way, the building process, for whatever reasons, does not
    detect the yaml (pyaml) package installed in the environment and global.
    I have to find out if `yaml` is part of the python standard library.
    The package I'm trying to upload is `live/projects/hi`.

--- live/projects/hi/hi/__meta__.py
+++ live/projects/hi/hi/__meta__.py
@@ -1,5 +1,5 @@
 __author__ = "Simon Nganga"
-__author_email__ = "happyfortunes5052@gmail.com"
+__author_email__ = "theedushbag@gmail.com"
 __maintainer__ = __author__
 __maintainer_email__ = __author_email__
 __version__ = "0.1"
--- live/projects/hi/setup.py
+++ live/projects/hi/setup.py
@@ -2,26 +2,21 @@ from pathlib import Path
 
 import hi.__meta__ as meta
 import setuptools
-import yaml
 
 WORKING_DIR = Path(__file__).parent
-CONFIG = WORKING_DIR / "config.yml"
 README = WORKING_DIR / "README.md"
-
-with CONFIG.open() as file:
-    config = yaml.safe_load(file)
 long_description = README.read_text()
 
 setuptools.setup(
-    name=config["package_name"],
+    name='hi',
     version=meta.__version__,
-    description=config.get("description", ""),
+    description='Commonly used hi and say_hi functions.',
     long_description=long_description,
     author=meta.__author__,
     author_email=meta.__author_email__,
-    packages=config["packages"],
-    requires=config.get("requirements", []),
+    packages=['hi'],
+    requires=['click', 'rich'],
     maintainer=meta.__maintainer__,
     maintainer_email=meta.__maintainer_email__,
-    keywords=config.get("keywords", []),
+    keywords=['say_hi', 'hi', 'hello', 'hi python'],
 )
