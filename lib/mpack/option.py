a2395df5db4fe9d2e03be4ce6e9a76ed7f0da3ad
Simon Nganga <happyfortunes5052@gmail.com>
2023-11-18 23:39:16 +0300

    Wrote my first test.
    Never used pytest before, my first time. It was amazing thinking of the
    different cases to test in the software. I wrote test for my
    module`mpack.option` and the test file is `live/python/option_test.py`.
    Turns out NotImplemented and NotImplementedError are two very different
    objects in python. NotImplemented is a status variable while
    NotImplementedError is the actual error that can be raised while the
    former is meant to be returned as a signal that the function was not
    called as the logic is not implemented.
    The test was written using pytest and to execute it use:
    $ pytest option_test.py
    
    All tests passed, amazingly.

--- lib/mpack/option.py
+++ lib/mpack/option.py
@@ -1,47 +1,56 @@
 import typing as ty
 from typing import Any
 
-__all__ = 'Option', 'NONE', 'NONETYPE'
+__all__ = "Option", "NONE", "NONETYPE"
 
-def _scrub_class(Class: ty.Type, val = None):
+
+def _scrub_class(Class: ty.Type, val=None):
     Class.__new__ = lambda *_, **__: val
     Class.__init__ = lambda *_, **__: None
-    if not hasattr(Class, '__eq__'):
+    if not hasattr(Class, "__eq__"):
         Class.__eq__ = lambda s, o: s is o
+
 
 def _single_none(Class: ty.Type):
     instance = None
+
     def get_instance(*args, **kwargs):
         nonlocal instance
         if instance is None:
             instance = Class(*args, **kwargs)
             _scrub_class(Class, instance)
         return instance
+
     return get_instance
 
+
 @_single_none
-class NONETYPE:
+class _NONETYPE:
     def __str__(self) -> str:
-        return '<NONE>'
+        return "<NONE>"
+
+    def __bool__(self) -> bool:
+        return False
 
     def __setattr__(self, _: str, __: Any) -> None:
-        raise NotImplemented
+        raise NotImplementedError
 
     def __delattr__(self, _: str) -> None:
-        raise NotImplemented
+        raise NotImplementedError
 
     def __getattribute__(self, _: str) -> Any:
-        raise NotImplemented
+        raise NotImplementedError
 
     def __init_subclass__(cls) -> None:
-        raise NotImplemented
+        raise NotImplementedError
 
     __repr__ = __str__
 
 
-NONE: NONETYPE = NONETYPE()
+NONE: _NONETYPE = _NONETYPE()
+NONETYPE: ty.Type = type(NONE)
 _T = ty.TypeVar("_T")
-_Optional: ty.TypeAlias = ty.Union[_T, NONETYPE]
+_Optional: ty.TypeAlias = ty.Union[_T, _NONETYPE]
 
 
 class Option(ty.Generic[_T]):
@@ -56,25 +65,33 @@ class Option(ty.Generic[_T]):
 
     def value(self) -> _T:
         "This function may throw[ValueError] if it holds a None."
-        if self.has_value(): return self._val
+        if self.has_value():
+            return self._val
         raise ValueError("Empty Option")
 
     def value_or(self, value: _T) -> _T:
         return self._val if self.has_value() else value
 
     def transform(self, func: ty.Callable[[_T], _Optional[_T]]) -> ty.Self:
-        return self.__class__(func(self._val)) if self.has_value() else self.__class__(self._val)
+        return (
+            self.__class__(func(self._val))
+            if self.has_value()
+            else self.__class__(self._val)
+        )
 
     def or_else(self, func: ty.Callable[[], ty.Any]) -> ty.Self:
-        if not self.has_value(): func()
+        if not self.has_value():
+            func()
         return self
 
     def and_then(self, func: ty.Callable[[_T], ty.Any]) -> ty.Self:
-        if self.has_value(): func(self._val)
+        if self.has_value():
+            func(self._val)
         return self
 
-    def reset(self) -> None:
-        self._val = NONE
+    def reset(self, value: _Optional[_T] = NONE) -> ty.Self:
+        self._val = value
+        return self
 
     def __str__(self) -> str:
         return f"Option(value={self._val!r})"
