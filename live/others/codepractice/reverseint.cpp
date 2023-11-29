c3526a6d694274d08488c019b62ee959f31ae4c2
Simon Nganga <theedushbag@gmail.com>
2023-11-29 23:58:23 +0300

    Learning on Bash shopt command
    Today I looked into the shopt command and the different flags it holds.
    I've not understood all but a few that will definately help me a lot
    when turning off/on different fetures of bash.
    The few options learned are:
    1. globstar
    2. nullglob
    3. interactive_comments
    4. dotglob
    5. nocaseglob
    6. nocasematch
    7. failglob
    8. autocd

--- live/others/codepractice/reverseint.cpp
+++ live/others/codepractice/reverseint.cpp
@@ -3,13 +3,39 @@
 #include <vector>
 #include <limits>
 #include <iomanip>
+#include <string_view>
+
+/*
+Given a signed 32-bit integer x, return x with
+its digits reversed. If reversing x causes the
+value to go outside the signed 32-bit integer
+range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you
+to store 64-bit integers (signed or unsigned).
+
+Example 1:
+  Input: x = 123
+  Output: 321
+
+Example 2:
+  Input: x = -123
+  Output: -321
+
+Example 3:
+  Input: x = 120
+  Output: 21
+
+Constraints:
+  -2^31 <= x <= 2^31 - 1
+*/
 
 class Solution {
 public:
   static int digitCounter(int x) {
     if (x == 0) return 0;
     auto val = std::log10(x);
-    if (std::nan(val) or std::isinf(val)) return 0;
+    if (std::isnan(val) or std::isinf(val)) return 0;
     return int(std::floor(val + 1));
   }
   static int getIndexDigit(int index, int number) {
@@ -19,19 +45,13 @@ public:
     bool isNegative = (x < 0);
     x = std::abs(x);
     long portion;
-    int result{ }, digit, x_len{ digitCounter(x) };
-    // std::cout << "Max: " << std::numeric_limits<int>::max() << " | x: " << x << '\n';
-    for (int index = 0; index < x_len; ++index) {
-      digit = getIndexDigit(index, x);
-      portion = std::pow(10, x_len - index - 1) * digit;
+    int result{ }, x_len{ digitCounter(x) }, index { }, rindex { x_len - 1 };
+    for (; index < x_len; ++index, --rindex) {
+      portion = std::pow(10, rindex) * getIndexDigit(index, x);
       if (portion > std::numeric_limits<int>::max()) return 0;
       result += portion;
       if (result < 0) return 0;
-      // std::cout << "Current: " << std::setw(x_len) << result
-      //     << " | Digit: " << digit <<  " | portion: "
-      //     << std::setw(x_len) << portion << '\n';
     }
-    if (result < 0) return 0;
     if (isNegative) result = -result;
     if (isNegative && result >= 0) return 0;
     return result;
@@ -41,16 +61,28 @@ public:
 
 auto main(int argc, char **argv) -> int {
   if (argc == 1) {
+    std::cerr << "Usage: " << argv[0] << " [numbers...]\n"
+      << "Example: " << argv[0] << " 4321 -98767 1023456789"
+      << R"(
+Output:
+  String: "1234"       | Input: 1234         | Output: 4321        
+  String: "-98767"     | Input: -98767       | Output: -76789      
+  String: "1023456789" | Input: 1023456789   | Output: 0           
+)";
     std::exit(1);
   }
   std::vector<int> numbers;
-  for (int i = 1; i < argc; i++)
+  std::size_t maxlen = 0;
+  for (int i = 1; i < argc; i++) {
     numbers.push_back(std::atoi(argv[i]));
-    int i = 0;
-  for (int number : numbers) {
-    i++;
-    std::cout << "String: " << argv[i]
-      << " | Input: " << number
-      << " | Output: " << Solution::reverse(number) << '\n';
+    maxlen = std::max(std::string_view(argv[i]).size(), maxlen);
   }
+  maxlen += 2;
+  int i{ 1 };
+  std::vector<int>::const_iterator iter{ numbers.cbegin() };
+  for (; iter != numbers.cend(); ++iter, ++i)
+    std::cout << std::left
+    << "String: " << std::setw(maxlen) << std::quoted(argv[i])
+    << " | Input: " << std::setw(maxlen) << *iter
+    << " | Output: " << std::setw(maxlen) << Solution::reverse(*iter) << '\n';
 }
