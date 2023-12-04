390349dcad33f9af967c3c476a36d91deb987da7
Simon Nganga <theedushbag@gmail.com>
2023-12-04 22:40:46 +0300

    Made my pow a little bit faster.
    So, I took a long walk around school thinking about the my
    implementation of the pow function I did early this morning and it
    occurred to me that I can improve the performance by getting rid of
    the recursion and the inner for loops in those recursions by saving to
    the cache the partial product of the whole product to the nth term
    instead of recalculating it whenever needed, this is what the for loops
    did, so instead of saving the nth terms, I save the product of the first
    n terms, hence no need for recalculation and for-loops, it was a bit
    tricky but I got it, the new algorithm is now faster the earlier one, it
    is even faster as the exponent grows, the higher the exponent the faster
    it is and it now truly works in O(log(N)), I think the earlier one was
    O(N*log(N)). I don't exactly know the difference but if I had to take a
    guess, due to the internal for-loops which all add up to greater than N,
    then it has to be N*log(N), though I will look into it later.
    HappyDays (*_*).

--- live/python/general_/pow.py
+++ live/python/general_/pow.py
@@ -31,28 +31,47 @@ def _create_pow_map(base: float, exp: int) -> tuple[float, dict[int, float], int
         temp *= temp
     return base, mapping, exp - (2**rounds - 1)
 
+def _create_growing_pow_map(base: float, exp: int) -> tuple[float, dict[int, float], int]:
+    cache: dict[int, float] = {}
+    rounds= _m.floor(_m.log2(exp))
+    temp, base, exp = base, 1, exp - (2 ** rounds - 1)
+    for round in range(rounds):
+        base *= temp
+        temp *= temp
+        cache[round] = base
+    return base, cache, exp
+
 
 def _compute_from_map(mapping: dict[int, float], exp: int) -> float:
     base = mapping[0]
-    if exp == 0:
-        return 1
-    elif exp == 1:
-        return base
+    if exp == 0: return 1
+    elif exp == 1: return base
     rounds = _m.floor(_m.log2(exp))
     for round in range(1, rounds):
         base *= mapping[round]
-    exp -= 2**rounds - 1
+    exp -= (2 ** rounds - 1)
     return base * _compute_from_map(mapping, exp)
 
 
+def _compute_from_growing_map(cache: dict[int, float], exp: int) -> float:
+    base = cache[0]
+    while exp > 1:
+        rounds = _m.floor(_m.log2(exp))
+        base *= cache[rounds - 1]
+        exp -= (2 ** rounds - 1)
+    return base
+
 def int_exp(base: float, exp: int, /) -> float:
-    if exp == 0:
-        return 1
-    elif exp == 1:
-        return base
+    if exp == 0: return 1
+    elif exp == 1: return base
     base, map, exp = _create_pow_map(base, exp)
     return base * _compute_from_map(map, exp)
 
+def int_exp_growing(base: float, exp: int, /) -> float:
+    if   exp == 0: return 1
+    elif exp == 1: return base
+    base, cache, exp = _create_growing_pow_map(base, exp)
+    return base * _compute_from_growing_map(cache, exp)
 
 def int_exp_denom(
     x: float, n: int, /, r: _Range = _default_range, *, factor: float = DECAY_FACTOR
@@ -99,7 +118,8 @@ def test_int_exp():
         for j in range(1, 1000, 3):
             powered = i**j
             assert powered == int_exp(i, j)
-            assert powered == _basic_pow(i, j)
+            # assert powered == _basic_pow(i, j)
+            assert powered == int_exp_growing(i, j)
 
 
 def average(times: list[TimeitResult]) -> tuple[float, float]:
@@ -107,38 +127,33 @@ def average(times: list[TimeitResult]) -> tuple[float, float]:
     return (total / len(times)), total
 
 
-def time_basic_vs_int_exp():
+def pow_benchmarks():
     from sys import set_int_max_str_digits  # type: ignore
 
-    set_int_max_str_digits(5000)
+    set_int_max_str_digits(10000)
     from tqdm import tqdm
 
     def run_test(power) -> tuple[float, float]:
         return average(
-            [power(i, j) for i in tqdm(range(1, 1000, 2)) for j in range(1, 500)]
+            [power(i, j) for i in tqdm(range(100, 10000, 3)) for j in range(1, 1000, 2)]
         )
 
     basic_pow = timer(_basic_pow)
     int_exp_pow = timer(int_exp)
-    power_pow = timer(power)
+    int_exp_gpow = timer(int_exp_growing)
+    # power_pow = timer(power)
     builtin_pow = timer(pow)
     bpow_avg, bpow_total = run_test(builtin_pow)
+    iexp_gavg, iexp_gtotal = run_test(int_exp_gpow)
     iexp_avg, iexp_total = run_test(int_exp_pow)
-    power_pow_total, power_pow_avg = run_test(power_pow)
+    # power_pow_total, power_pow_avg = run_test(power_pow)
     basic_avg, basic_total = run_test(basic_pow)
-    print(
-        f"BuiltinPow took a total of {bpow_total} total and {bpow_avg} average seconds."
-    )
-    print(
-        f"PowerFloat took a total of {power_pow_total} total and {power_pow_avg} average seconds."
-    )
-    print(
-        f"IntExpPow took a total of {iexp_total} total and {iexp_avg} average seconds."
-    )
-    print(
-        f"BasicPower took a total of {basic_total} total and {basic_avg} average seconds."
-    )
+    print(f"BuiltinPow took a total of {bpow_total} total and {bpow_avg} average seconds.")
+    # print(f"PowerFloat took a total of {power_pow_total} total and {power_pow_avg} average seconds.")
+    print(f"IntExpPowGrowing took a total of {iexp_gtotal} total and {iexp_gavg} average seconds.")
+    print(f"IntExpPow took a total of {iexp_total} total and {iexp_avg} average seconds.")
+    print(f"BasicPower took a total of {basic_total} total and {basic_avg} average seconds.")
 
 
-# if __name__ == "__main__":
-#     time_basic_vs_int_exp()
+if __name__ == "__main__":
+    pow_benchmarks()
