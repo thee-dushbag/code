c1337e22371f47a57cdb71180eed7e31686fb602
Simon Nganga <theedushbag@gmail.com>
2023-12-10 02:08:27 +0300

    Read on last third of asyncio chapter 9.
    Django is the king-kong of web frameworks, pretty big and strong. I
    will stick to aiohttp for its simplicity, flexibility and plugin
    architecture. I had to recreate the django project since it had a lot of
    issues from not loading the startup django page to migration bugs.

--- live/others/concurrency-in-python-with-asyncio-master/chapter_09/ldjango/async_api/views.py
+++ live/others/concurrency-in-python-with-asyncio-master/chapter_09/ldjango/async_api/views.py
@@ -1,12 +1,13 @@
-import asyncio
-from datetime import datetime
-
-import aiohttp
-from aiohttp import ClientSession
+from asgiref.sync import sync_to_async
+from asgiref.sync import async_to_sync
+from django.http import HttpResponse
 from django.shortcuts import render
+from datetime import datetime
+from functools import partial
+import aiohttp, asyncio
 
 
-async def get_url_details(session: ClientSession, url: str):
+async def get_url_details(session: aiohttp.ClientSession, url: str):
     start_time = datetime.now()
     response = await session.get(url)
     response_body = await response.text()
@@ -20,14 +21,10 @@ async def get_url_details(session: ClientSession, url: str):
 
 async def make_requests(url: str, request_num: int):
     async with aiohttp.ClientSession() as session:
-        requests = [get_url_details(session, url) for _ in range(request_num)]
+        requests = (get_url_details(session, url) for _ in range(request_num))
         results = await asyncio.gather(*requests, return_exceptions=True)
-        failed_results = [
-            str(result) for result in results if isinstance(result, Exception)
-        ]
-        successful_results = [
-            result for result in results if not isinstance(result, Exception)
-        ]
+        failed_results = [str(r) for r in results if isinstance(r, Exception)]
+        successful_results = [r for r in results if not isinstance(r, Exception)]
         return {
             "failed_results": failed_results,
             "successful_results": successful_results,
@@ -35,16 +32,12 @@ async def make_requests(url: str, request_num: int):
 
 
 async def requests_view(request):
+    loop = asyncio.get_running_loop()
+    print(f"EventLoopID: {hex(id(loop))}")
     url: str = request.GET["url"]
     request_num: int = int(request.GET["request_num"])
     context = await make_requests(url, request_num)
     return render(request, "async_api/requests.html", context)
-
-
-from functools import partial
-
-from asgiref.sync import sync_to_async
-from django.http import HttpResponse
 
 
 def sleep(seconds: int):
@@ -56,15 +49,12 @@ def sleep(seconds: int):
 async def sync_to_async_view(request):
     sleep_time: int = int(request.GET["sleep_time"])
     num_calls: int = int(request.GET["num_calls"])
-    thread_sensitive: bool = request.GET["thread_sensitive"] == "True"
+    thread_sensitive: bool = request.GET["thread_sensitive"].lower() == "true"
     function = sync_to_async(
         partial(sleep, sleep_time), thread_sensitive=thread_sensitive
     )
     await asyncio.gather(*[function() for _ in range(num_calls)])
     return HttpResponse("")
-
-
-from asgiref.sync import async_to_sync
 
 
 def requests_view_sync(request):
