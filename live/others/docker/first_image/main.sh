b38eb1dd59802a329a98ab113f73ccb6fd48978f
Simon Nganga <theedushbag@gmail.com>
2023-12-23 21:01:40 +0300

    Created first image and published on docker
    I've been reading some manning book on docker lately, currently just
    started chapter 7, I have some tricks up my sleeves about docker and
    used them to create my first image, it is a simple family greeter app
    written in bash, could have used any other language from c, c++, python,
    javascript etc but then I would have had to install a whole bunch of
    stuff, like nodejs, g++, clang, python which would have made the image
    unnecessarily big, and remember, sh is default installed in busybox, my
    base image.
    To run it, pass environment variables NAME, SISTER, MUM, DAD where NAME
    is your name and the rest, your family.
    I have published the image on my dockerhub account, theedushbag with the
    name 'fello' for 'hello family'. Boooyah (*_*)

---
+++ live/others/docker/first_image/main.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sister=${SISTER:-<NO_NAME>}
+mum=${MUM:-<NO_NAME>}
+dad=${DAD:-<NO_NAME>}
+name=${NAME:-<NO_NAME>}
+
+echo Hello There?
+echo My name is $name
+echo my sister is $sister,
+echo my mum is $mum and
+echo my dad is $dad.
+echo Now tell me about you!
