6db56c4d197f9d9576bda28b810978fa972c1b09
Simon Nganga <theedushbag@gmail.com>
2023-12-23 21:01:40 +0300

    Created first image and published on docker
    I've been reading some manning book on docker lately, currently just
    started chapter 7, I have some tricks up my sleeves about docker and
    used them to create my first image, it is a simple family greeter app
    written in bash, could have used any other language from c, c++, python,
    javascript etc but then I would have had to install a whole bunch of
    stuff, like nodejs, g++, clang, python which would have made the image
    unnecessarily big, and remember, sh is default installed in busybox, my
    base image.
    To run it, pass environment variables NAME, SISTER, MUM, DAD where NAME
    is your name and the rest, your family.
    I have published the image on my dockerhub account, theedushbag with the
    name 'fello' for 'hello family'. Boooyah (*_*)

--- live/others/codepractice/easytier.py
+++ live/others/codepractice/easytier.py
@@ -1,7 +1,8 @@
-from typing import Counter
+import typing as ty
+
 
 class Solution:
-    braces = {'(':')', '{':'}', '[':']'}
+    _braces = {"(": ")", "{": "}", "[": "]"}
 
     def maxProductDifference(self, nums: list[int]) -> int:
         """
@@ -33,8 +34,9 @@ class Solution:
         """
         trace = []
         for brace in s:
-            if brace in self.braces: trace.append(brace)
-            elif not trace or self.braces[trace.pop()] != brace:
+            if brace in self._braces:
+                trace.append(brace)
+            elif not trace or self._braces[trace.pop()] != brace:
                 return False
         return not trace
 
@@ -47,8 +49,7 @@ class Solution:
         the letters of a different word or phrase, typically
         using all the original letters exactly once.
         """
-        return Counter(s) == Counter(t)
-
+        return ty.Counter(s) == ty.Counter(t)
 
     def destCity(self, paths: list[list[str]]) -> str:
         """
@@ -61,3 +62,38 @@ class Solution:
         any loop, therefore, there will be exactly one destination city.
         """
         return ({c for ct in paths for c in ct} - {o for o, _ in paths}).pop()
+
+    def maxScore(self, s: str) -> int:
+        """
+        Given a string s of zeros and ones, return the maximum score
+        after splitting the string into two non-empty substrings (i.e.
+        left substring and right substring).
+
+        The score after splitting a string is the number of zeros in
+        the left substring plus the number of ones in the right substring.
+        """
+        max_score = score = (s[0] == "0") + s[1:].count("1")
+        for bit in map(int, s[1:-1]):
+            score += not bit or -1
+            if score > max_score:
+                max_score = score
+        return max_score
+
+    _step: ty.Callable[[str], tuple[int, int]] = staticmethod(
+        dict(
+            N=(0, 1),
+            S=(0, -1),
+            E=(1, 0),
+            W=(-1, 0),
+        ).__getitem__
+    )
+
+    def isPathCrossing(self, path: str) -> bool:
+        current: tuple[int, int] = 0, 0
+        passed: set[tuple[int, int]] = {current}
+        for step in map(self._step, path):
+            current = current[0] + step[0], current[1] + step[1]
+            if current in passed:
+                return True
+            passed.add(current)
+        return False
