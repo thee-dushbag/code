0d08f199d2a687f136ef6ca582f1c235c3a6cb84
Simon Nganga <theedushbag@gmail.com>
2024-02-05 17:23:55 +0300

    Background Work. Read on math, c++ templates, etc.
    Once upon a time, I tried to read later chapters of the `C++ template
    metaprogramming` by David, specifically the topic on `typelists` and
    operations on them, I failed miserably. But the content kept running
    around my brain since them and yesterday, it snapped, `that is what is
    was`. Out of nowhere I understood the contents of the type list and made
    an effort to write my own from scratch, not bragging or anything, it
    works perfectly. The most intense operation to implement was the
    reverse, to reverse the type list. Partial specializations were also a
    mistery that uncovered yesterday.
    Otherwise, today I did some linear algebra, played around with lazy
    evaluatins, which are pretty cool, they are used in sympy library. Can't
    really say how, but I made a simple one, thanks to crafting interpreters
    introduction to tree walk interpreteros that really helped in the
    understanding of lazy evaluations.
    I've always wondered how python knows when an operator is not defined
    for, example __add__, where it will then try to call, for this case,
    __radd__. I've experimented on a few things here and there and I've come
    to a conclusion, if the operator, example __add__, returns the
    NotImplemented object then __radd__ is tried, if it also returns the
    NotImplemented object, an error, TypeError, is raised that the +
    operation is not defined for the operands provided. Cool. This just blew
    a whole lot of ideas in my head.

--- lib/include/snn_type_list.hpp
+++ lib/include/snn_type_list.hpp
@@ -3,12 +3,70 @@
 
 #include <type_traits>
 
-namespace snn::typelist
-{
+namespace snn {
+  namespace list {
+    template<typename ...types>
+    struct typelist;
+
+    template<typename list>
+    struct pop_left;
+    template<typename _type, typename ...types>
+    struct pop_left<typelist<_type, types...>> {
+      using type = typelist<types...>;
+    };
+
+    template<typename list>
+    struct pop_right;
+    template<typename last, typename ...types>
+    struct pop_right<typelist<types..., last>> {
+      using type = typelist<types...>;
+    };
+
+    template<typename list>
+    struct first_type;
+    template<typename first, typename ...types>
+    struct first_type<typelist<first, types...>> {
+      using type = first;
+    };
+
+    template<typename type, typename list>
+    struct push_left;
+    template<typename _type, typename ...types>
+    struct push_left<_type, typelist<types...>> {
+      using type = typelist<_type, types...>;
+    };
+
+    template<typename type, typename list>
+    struct push_right;
+    template<typename _type, typename ...types>
+    struct push_right<_type, typelist<types...>> {
+      using type = typelist<types..., _type>;
+    };
+
+    template<typename list>
+    struct reverse;
+    template<typename ...types>
+    struct reverse<typelist<types...>> : push_right<typename first_type<typelist<types...>>::type,
+      typename reverse<typename pop_left<typelist<types...>>::type>::type> { };
+
+    template<>
+    struct reverse<typelist<>> {
+      using type = typelist<>;
+    };
+
+    template<typename type, typename list>
+    struct is_one_of;
+    template<typename type, typename ...types>
+    struct is_one_of<type, typelist<types...>>
+      : std::bool_constant<(std::is_same_v<type, types> || ...)> { };
+  }
+
+  namespace typelist {
     template <typename T, T val>
-    struct integral_constant
-    { using type = T;
-      constexpr static T value = val; };
+    struct integral_constant {
+      using type = T;
+      constexpr static T value = val;
+    };
 
     template <bool b>
     using bool_constant = integral_constant<bool, b>;
@@ -17,23 +75,26 @@ namespace snn::typelist
     using false_type = bool_constant<false>;
 
     template <typename... Types>
-    struct type_list {};
+    struct type_list { };
 
     namespace __detail {
-        template<bool>
-        struct _conditional
-        { template<typename Type, typename>
-        using type = Type; };
+      template<bool>
+      struct _conditional {
+        template<typename Type, typename>
+        using type = Type;
+      };
 
-        template<>
-        struct _conditional<false>
-        { template<typename, typename Type>
-        using type = Type; };
+      template<>
+      struct _conditional<false> {
+        template<typename, typename Type>
+        using type = Type;
+      };
     }
 
     template<bool b, typename tType, typename fType>
-    struct if_then_else
-    { using type = typename __detail::_conditional<b>::type<tType, fType>; };
+    struct if_then_else {
+      using type = typename __detail::_conditional<b>::type<tType, fType>;
+    };
 
     template<bool b, typename TrueType, typename FalseType>
     using if_then_else_t = if_then_else<b, TrueType, FalseType>::type;
@@ -43,11 +104,11 @@ namespace snn::typelist
 
     template <typename Type, typename... Types>
     struct is_one_of<Type, type_list<Types...>>
-        : bool_constant<(std::is_convertible_v<Type, Types> || ...) || (std::is_constructible_v<Types, Type> || ...)>
-    {};
+      : bool_constant<(std::is_convertible_v<Type, Types> || ...) || (std::is_constructible_v<Types, Type> || ...)> { };
 
     template <typename Type, typename... Types>
     constexpr bool is_one_of_v = is_one_of<Type, Types...>::value;
+  }
 }
 
 #endif //__SNN_TYPE_LIST_HPP_
--- live/math/linear_alg/main.py
+++ live/math/linear_alg/main.py
@@ -1,13 +1,106 @@
-from vectors import Vector, dot_product_algebraic, vector_angle
+import lazy as z
+from vectors import (
+    Vector,
+    dot_product_algebraic,
+    vector_angle,
+    perpendicular,
+    cross_product_algebraic,
+    cross_product_geometric,
+    project,
+    is_parallel,
+)
 
 
-def main():
+def main1():
     v = Vector([1, 2, 3])
     print(f"{v}.magnitude = {v.magnitude}")
     u = Vector([4, 5, 6])
     print(f"dot({v}, {u}) = {dot_product_algebraic(v, u)}")
     print(f"dot({u}, {v}) = {dot_product_algebraic(u, v)}")
-    print(f'angle({v}, {u}) = {vector_angle(v, u)}')
+    print(f"angle({v}, {u}) = {vector_angle(v, u)}")
+
+
+def main2():
+    pb = Vector([17, 2, -13])
+    upb = pb.unit()
+    b = Vector([4, 5, 6])
+    cpb = perpendicular(b, Vector([1, 1, 1]))
+    ucpb = cpb.unit()
+    print(pb, cpb)
+    print(upb, ucpb, sep="\n")
+    print(ucpb == upb)
+
+
+def main3():
+    a = Vector([1, 2, 3])
+    b = Vector([4, 5, 6])
+    ab = cross_product_algebraic(a, b)
+    print(ab, ab.magnitude)
+    abg = cross_product_geometric(a, b, ab.unit())
+    print(abg, abg.magnitude)
+
+
+def main4():
+    a = Vector([1, 2, 3])
+    b = Vector([4, 5, 6])
+    pb = perpendicular(b, Vector([1, 1, 1]))
+    height = project(a, pb)
+    c_area = height * b.magnitude
+    area = cross_product_geometric(a, b)
+    print(c_area, area)
+
+
+def main5():
+    a = Vector([1, 2, 3])
+    b = Vector([4, 5, 6])
+    ab = cross_product_algebraic(a, b)
+    ba = cross_product_algebraic(b, a)
+    print(ab, ba, ba * -1, sep="\n")
+    assert ab == -1 * ba
+
+
+def main6():
+    a = Vector([2, 2, 2])
+    b = Vector([3, 3, 3])
+    c = Vector([1, 2, 3])
+    print(f"{a.unit()=}")
+    print(f"{b.unit()=}")
+    print(f"{c.unit()=}")
+    abp = is_parallel(a, b)
+    acp = is_parallel(a, c)
+    bcp = is_parallel(b, c)
+    print(f"{a} // {b}: {abp}")
+    print(f"{a} // {c}: {acp}")
+    print(f"{b} // {c}: {bcp}")
+
+
+def main7():
+    b = z.Vector(map(z.number, [2, 2, 2]))
+    print(b.unit())
+    a = z.Vector(map(z.number, [1, 2, 3]))
+    b = z.Vector(map(z.number, [4, 5, 6]))
+    print(z.absolute(a))
+    print(a.magnitude())
+    print(a.magnitude() == z.absolute(a))
+    print(z.evaluate(a.magnitude()) == z.evaluate(z.absolute(a)))
+
+
+def main():
+    a = Vector([1, 2, 3])
+    b = Vector([4, 5, 6])
+    ab = cross_product_algebraic(a, b)
+    c = perpendicular(b, a)
+    # print(ab, c)
+    # print(ab.unit(), c.unit(), sep="\n")
+    a = Vector([4, 6])
+    b = Vector([8, 7])
+    pa = perpendicular(a, Vector([-5, -7]))
+    pb = perpendicular(b, Vector([1, 99]))
+    print(dot_product_algebraic(pa, a))
+    print(dot_product_algebraic(pb, b))
+    scale = dot_product_algebraic(a, pb)
+    print(scale / a.magnitude)
+    print(scale / pb.magnitude)
 
 
 if __name__ == "__main__":
