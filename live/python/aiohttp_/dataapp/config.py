5c349f139a704233a2a283e76a0316ae370d6e74
Simon Nganga <theedushbag@gmail.com>
2024-03-22 23:53:27 +0300

    Dataapp Review and lots of math
    Took a random project, dataapp, created using aiohttp and reviewed it.
    To say the truth, it is the one project in this repo that is bearable
    given my current capability. I think this is one of the projects I made
    with the aim of understanding and using bit masks. The masks represented
    permission levels, etc, readon the project code for comprehension.
    Otherwise, I've done lots of math today, none involved programming,
    school work. As I did all that, an idea was born, like the last one, it
    seems pretty important and best kept as a secret. I've thought about it
    for a significant amount of time and here I am adding it to my diary; after this. NIGHT.

--- live/python/aiohttp_/dataapp/config.py
+++ live/python/aiohttp_/dataapp/config.py
@@ -3,4 +3,5 @@ import typing as ty
 
 WORKING_DIR = Path(__file__).parent
 USERSDATA_FILE = WORKING_DIR / 'usersdata.yaml'
-PERSIST_TYPE: ty.Literal['yaml', 'json'] = 'yaml'
+PersistTypes = ty.Literal['yaml', 'json']
+PERSIST_TYPE: PersistTypes = 'yaml'
